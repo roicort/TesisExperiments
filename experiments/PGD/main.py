6d8ab74d27aa674a85148a38c1ca85b7ae96c58e
Rodrigo <28023409+roicort@users.noreply.github.com>
2020-08-03 14:36:35 -0500

    New Struct

---
+++ experiments/PGD/main.py
@@ -0,0 +1,12 @@
+from wasabi import msg
+
+from preprocessing import graph2edges
+from runner import runpgd
+from clustering import clustering
+
+#log = graph2edges('../../datasets/twitter','input/')
+#msg.info(log)
+log = runpgd('input/',"output/")
+msg.info(log)
+log = clustering('output')
+msg.info(log)
---
+++ experiments/graph2vec/main.py
@@ -0,0 +1,12 @@
+from wasabi import msg
+
+from preprocessing import graph2json
+from runner import rungraph2vec
+from clustering import clustering
+
+#log = graph2json('../../datasets/twitter','input/')
+#msg.info(log)
+log = rungraph2vec('input/',"output/")
+msg.info(log)
+log = clustering('output/')
+msg.info(log)
