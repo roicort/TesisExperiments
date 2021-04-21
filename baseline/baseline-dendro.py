915a6fce8588e3e1389d83123e3d18f14ab8f40a
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2021-04-20 20:35:40 -0500

    V2.0

--- baseline/baseline-dendro.py
+++ baseline/baseline-dendro.py
@@ -8,10 +8,30 @@ from wasabi import msg
 import pickle
 import networkx as nx
 
-def main(read_path):
+import plotly.figure_factory as ff
 
-    df = pd.read_csv(read_path)
-    df.drop(columns=["Label","Unnamed: 0"],inplace=True)
-    print(df)
+from sklearn.preprocessing import MinMaxScaler 
+
+import scipy.stats as stats
+from scipy.cluster import hierarchy
+
+def main(read_path,save_path="results/"):
+
+    X = pd.read_csv(read_path)
+    X.drop(columns=["Label","Unnamed: 0"],inplace=True)
+
+    labels = list(X["Network"])
+    X = X.drop(columns=["Network"],inplace=False)
+
+    print(labels)
+    print(X)
+
+    X = np.array(X)
+
+    for method in tqdm(['single','complete','average','weighted','centroid','median','ward']):
+        fig = ff.create_dendrogram(X, orientation='left', labels=labels, linkagefun=lambda alpha: hierarchy.linkage(alpha,method=method,optimal_ordering=True))
+        fig.update_layout(autosize=True) 
+        fig.update_layout(height=700) 
+        fig.write_html(save_path+"Baseline"+"-"+method.upper()+".dendrogram.html")
 
 main('baseline.csv')
