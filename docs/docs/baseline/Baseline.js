befc69bb97b907d45cc950c4d909ca3c84d51e60
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2021-04-22 15:25:16 -0500

    idk

---
+++ docs/docs/baseline/Baseline.js
@@ -0,0 +1,47 @@
+function json2table(json, classes) {
+  var cols = Object.keys(json[0]);
+  
+  var headerRow = '';
+  var bodyRows = '';
+  
+  classes = classes || '';
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  cols.map(function(col) {
+    headerRow += '<th>' + capitalizeFirstLetter(col) + '</th>';
+  });
+
+  json.map(function(row) {
+    bodyRows += '<tr>';
+
+    cols.map(function(colName) {
+      bodyRows += '<td>' + row[colName] + '</td>';
+    })
+
+    bodyRows += '</tr>';
+  });
+
+  return '<table class="' +
+         classes +
+         '"><thead><tr>' +
+         headerRow +
+         '</tr></thead><tbody>' +
+         bodyRows +
+         '</tbody></table>';
+}
+
+var txtFile = new XMLHttpRequest();
+txtFile.open("GET", "https://raw.githubusercontent.com/roicort/TesisGraphlets/master/baseline/baseline.csv", true);
+txtFile.onreadystatechange = function() {
+  if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (txtFile.status === 200) {  // Makes sure it's found the file.
+      txt = txtFile.responseText;
+      const json = CSVJSON.csv2json(txt, {parseNumbers: true});
+      document.getElementById('mytable').innerHTML = json2table(json, 'table');
+    }
+  }
+}
+txtFile.send(null);
---
+++ docs/docs/baseline/Baseline.md
@@ -0,0 +1,19 @@
+---
+layout: default
+title: Baseline
+nav_order: 3
+has_children: false
+permalink: docs/Baseline
+---
+
+# Baseline
+
+<br>
+
+<div id="mytable"></div>
+
+<iframe id="myframe"></iframe>
+
+<script src="https://cdn.jsdelivr.net/npm/csvjson-csv2json@5.0.6/csv2json.js"></script>
+<script src="Baseline.js"></script>
+<script src="iframe.js"></script>
