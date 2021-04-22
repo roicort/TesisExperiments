5525fd9bd7387b9e3203cd25125cc9d052f35eff
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2021-04-22 15:52:29 -0500

    iframes

---
+++ sync_iframes.sh
@@ -0,0 +1,8 @@
+
+
+cp baseline/results/*.html docs/docs/baseline/iframes
+
+cp embeddings/results/*.html docs/docs/embeddings/iframes
+
+cp GCDN/results/*.html docs/docs/gcdn/iframes
+cp GCDN/users/*.html docs/docs/gcdn/iframes
