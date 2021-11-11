312639ba11bccd9ebb0fadcd349c417cfe21cb5f
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2021-11-10 20:20:02 -0600

    Delete sync_iframes.sh

--- sync_iframes.sh
+++
@@ -1,8 +0,0 @@
-
-
-cp baseline/results/*.html docs/docs/baseline/iframes
-
-cp embeddings/results/*.html docs/docs/embeddings/iframes
-
-cp GCDN/results/*.html docs/docs/gcdn/iframes
-cp GCDN/users/*.html docs/docs/gcdn/iframes
